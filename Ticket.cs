06587ced1543efe0cee30bd66b1222d13f761e34
Mona Lisa <email@example.com>
2023-03-22 20:05:42 -0500

    [1.01] Initial Commit + Existing Files

---
+++ Ticket.cs
@@ -0,0 +1,20 @@
+public class Ticket
+{
+  public UInt64 TicketID { get; set; }
+  public string Summary { get; set; }
+  public string Status { get; set; }
+  public string Priority { get; set; }
+  public string Submitter { get; set; }
+  public string Assigned { get; set; }
+  public List<string> Watching { get; set; }
+  public string Severity { get; set; }
+
+ public Ticket()
+  {
+    Watching = new List<string>();
+  }
+  public string Display()
+    {
+      return $"Id: {TicketID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join(", ", Watching)}\nSeverity: {Severity}\n";
+    }
+}
